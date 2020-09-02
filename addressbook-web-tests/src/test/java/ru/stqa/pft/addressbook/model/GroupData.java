534feddc623bafe4f2fa9925d2864f79bfa3803d
ebaltinova <ebaltinova@issart.com>
2020-09-02 20:45:38 +0600

    4.10 группы
    Сравнение без id

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -8,8 +8,13 @@ public class GroupData {
     private final String footer;
     private int id;
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -29,6 +34,13 @@ public class GroupData {
         this.id = id;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
+    }
     public String getName() {
         return name;
     }
@@ -49,17 +61,4 @@ public class GroupData {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return id == groupData.id &&
-                Objects.equals(name, groupData.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, id);
-    }
 }
