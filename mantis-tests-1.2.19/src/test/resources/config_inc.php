ba4238a358c7c3bf4188952425befbcdc91c73a0
Elmira Baltinova <62242594+EBaltinova@users.noreply.github.com>
2020-11-17 13:38:26 +0600

    8.6 готово

--- mantis-tests-1.2.19/src/test/resources/config_inc.php
+++ mantis-tests-1.2.19/src/test/resources/config_inc.php
@@ -5,3 +5,5 @@ $g_database_name = 'bugtracker1.2.19';
 $g_db_username = 'root';
 $g_db_password = '';
 $g_signup_use_captcha = OFF;
+$g_phpMailer_method = PHPMAILER_METHOD_SMTP;
+$g_smtp_host = 'localhost';
